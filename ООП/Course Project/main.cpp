3a23f9596151ebf667341cb9a89dc012fc9ea4fb
Xflowbeats <ivan.korol.2007@bk.ru>
2021-05-12 20:13:58 +0300

    Add course project and book for MAT ANAL

---
+++ ООП/Course Project/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+class main
+{
+private:
+    /* data */
+public:
+    main(int a);
+    ~main();
+};
+
+main::main(/* args */)
+{
+}
+
+main::~main()
+{
+}
+
+
+int main(){
+    cout<<"HI"<<endl;
+    return 0;
+}
